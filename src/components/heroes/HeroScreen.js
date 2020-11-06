be1d86e8347bb7306e7b9cd1ef4e434da8858889
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-11-05 17:56:02 -0700

    Detalle por Heroe seleccionado

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeros';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
+
+  // Extrae los parametros desde la URL
+  const { heroeId } = useParams();
+  const heroe = getHeroById(heroeId);
+
+  if(!heroe) {
+    return <Redirect to="/" />
+  }
+
+  const handleReturn = () => {
+    history.length <= 2
+      ? history.push('/')
+      : history.goBack();
+  }
+
+  const {
+    publisher,
+    superhero,
+    alter_ego,
+    first_appearance,
+    characters
+  } = heroe;
+
   return (
-    <div>
-      <h1> Hero Page! </h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={ superhero }
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h2> { superhero } </h2>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego: </b> {alter_ego} </li>
+          <li className="list-group-item"> <b>Publisher: </b> {publisher}</li>
+          <li className="list-group-item"> <b>Fisrt appearance: </b> {first_appearance}</li>
+        </ul>
+
+        <h5> Characters: </h5>
+        <p> { characters } </p>
+
+        <button 
+          className="btn btn-success"
+          onClick={ handleReturn }  
+        >
+          Return
+        </button>
+      </div>
     </div>
   );
 }
